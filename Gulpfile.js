3e28f28df2acc8c7ebf048a5661c730ea69bb38c
inkering <inkering@gmail.com>
2017-07-15 17:44:08 -0400

    added comments

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,11 +3,18 @@
 // https://github.com/Inkering
 "use strict";
 
+// This gulpfile requires:
+// * gulp (duh!)
+// * gulp-sass
+// * gulp-autoprefixer
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-// create a seperate task for style compilation with scss/sass
+// create a seperate task for style compilation
+// * compiles sass files
+// * applies autoprefixer to them
+// * outputs in a standard ./css/
 gulp.task('styles', function() {
 	gulp.src('sass/**/*.scss')
 			.pipe(sass().on('error', sass.logError))
@@ -15,7 +22,7 @@ gulp.task('styles', function() {
 			.pipe(gulp.dest('./css/'));
 });
 
-// Create a default task that runs and watchs:
+// Create a main default task that runs and watchs:
 // * scss compilation
 gulp.task('default',function(){
 	gulp.watch('sass/**/*.scss',['styles']);
